ec7792b4fb4e31261d216efd01bb8241d5a4a3d3
Ting An Lai <tlai28@wisc.edu>
2019-10-21 01:44:25 -0400

    envelope package add test

--- hw05/Envelope.Rcheck/00_pkg_src/Envelope/R/envelope_func.R
+++ hw05/Envelope.Rcheck/00_pkg_src/Envelope/R/envelope_func.R
@@ -1,4 +1,5 @@
 #' Envelope function
+#'
 #' This is a function that helps you decide how much envelope money each child
 #' should receive in each family reasonably and fairly.
 #' @param x number of children in x family
@@ -8,31 +9,38 @@
 #' should each child receive in each family.
 #' @return a vector of envelope money for x and y family
 #' @example
-#' envelope(3, 4)
-#' envelope(5, 5)
-#' envelope(2, 2)
+#' envelope (3,4)
+#' envelope (5,5)
+#' envelope (2,2)
 #' @export
+
+
 envelope <- function(x=4, y=4, ...) {
-  a <- max(x, y)
-  b <- min(x, y)
-  gcd <- 1
-  if (a == b) {
-    print("each child receive $1000 in envelope")
-    return(1000)
+  a = max (x, y)
+  b = min (x, y)
+  gcd = 1
+
+  if (a==b) {
+    print ("each child receive $1000 in envelope")
+    return (1000)
     } else {
-      while (b == TRUE) {
-      a <- b
-      b <- a %% b
-      gcd <- a
+
+      while (b==TRUE) {
+      a = b
+      b = a%%b
+      gcd = a
       }
-    ap <- x / gcd
-    bp <- y / gcd
-    lcm <- ap * bp * gcd * 300
-    xreceive <- lcm / ap
-    yreceive <- lcm / bp
-    print(paste0("In x family, each child receive $", xreceive))
-    print(paste0("In y family, each child receive $", yreceive))
+
+    ap = x/gcd
+    bp = y/gcd
+    lcm = ap*bp*gcd*300
+    xreceive = lcm/ap
+    yreceive = lcm/bp
+
+    print (paste0("In x family, each child receive $", xreceive))
+    print (paste0("In y family, each child receive $", yreceive))
     }
-  ab <- c(xreceive, yreceive)
-  return(ab)
-  }
+  ab = c(xreceive, yreceive)
+  return (ab)
+}
+
